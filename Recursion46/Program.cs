1656479f7a2fb6784fa09ed68446befc76b1ad34
Dan Zilberstein <dzilbers@gmail.com>
2018-11-19 15:44:19 +0200

    fix 46

--- Recursion46/Program.cs
+++ Recursion46/Program.cs
@@ -18,11 +18,20 @@ namespace Recursion46
                         map[i, j] = 'x';
             return temp;
         }
-        static int neighboorsRecursion(char [,] map, int row, int col)
+        static int neighboorsRecursion(char[,] map, int row, int col)
         {
+            if (row < 0 || row >= map.GetLength(0) ||
+                col < 0 || col >= map.GetLength(1))
+                return 0;
             if (map[row, col] != 'x')
                 return 0;
-
+            map[row, col] = 'y';
+            return 1 +
+                neighboorsRecursion(map, row - 1, col) + // Up
+                neighboorsRecursion(map, row + 1, col) + // Down
+                neighboorsRecursion(map, row, col - 1) + // Left
+                neighboorsRecursion(map, row, col + 1);  // Right
+            /*
             int sum = 1;
             map[row, col] = 'y';
             // Up
@@ -38,6 +47,17 @@ namespace Recursion46
             if (col < map.GetUpperBound(1))
                 sum += neighboors(map, row, col + 1);
             return sum;
+            */
+        }
+
+        static void print(char[,]map)
+        {
+            for (int i = 0; i < map.GetLength(0); ++i)
+            {
+                for (int j = 0; j < map.GetLength(1); ++j)
+                    Console.Write(" " + map[i, j]);
+                Console.WriteLine();
+            }
         }
 
         static void Main(string[] args)
@@ -56,13 +76,21 @@ namespace Recursion46
             while (true)
             {
                 Console.WriteLine("Please enter row and column number:");
-                int col, row;
-                row = int.Parse(Console.ReadLine());
-                if (row == -1)
-                    return;
-                col = int.Parse(Console.ReadLine());
-                if (col == -1)
-                    return;
+                int col = 0, row = 0;
+                try
+                {
+                    row = int.Parse(Console.ReadLine());
+                    if (row == -1)
+                        return;
+                    col = int.Parse(Console.ReadLine());
+                    if (col == -1)
+                        return;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Bad input!");
+                    continue;
+                }
                 Console.WriteLine("For yard {1}-{2} there are {0} total neighboors",
                     neighboors(yards, row, col), row, col);
             }
