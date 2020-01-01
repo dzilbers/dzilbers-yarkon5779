76f75318fe352051cd3125fd693a1bc231728e64
Dan Zilberstein <dzilbers@gmail.com>
2020-01-01 08:14:00 +0200

    Queue fix with using GetValue from Node and added comments

--- Queue/Queue.cs
+++ Queue/Queue.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Chains;
 
+/// <summary>
+/// Allows using FIFO queues. This generic class is implemented using a chain
+/// and holds references to the first and to the last node in the chain.
+/// </summary>
 namespace Queue
 {
     public class Queue<T>
@@ -12,16 +16,27 @@ namespace Queue
         private Node<T> first;
         private Node<T> last;
 
+        /// <summary>
+        /// Initializes an empty FIFO queue instance
+        /// </summary>
         public Queue()
         {
             first = last = null;
         }
 
+        /// <summary>
+        /// Checks whether the queue is empty or not.
+        /// </summary>
+        /// <returns>true if the queue is empty and false otherwise</returns>
         public bool IsEmpty()
         {
             return first == null;
         }
 
+        /// <summary>
+        /// Insert a value into the queue
+        /// </summary>
+        /// <param name="value">the inserted value</param>
         public void Insert(T value)
         {
             Node<T> node = new Node<T>(value);
@@ -32,23 +47,35 @@ namespace Queue
             last = node;
         }
 
+        /// <summary>
+        /// Removes a value from the queue and returns it.
+        /// </summary>
+        /// <returns>the removed value</returns>
         public T Remove()
         {
-            T temp = first.GetInfo();
+            T temp = first.GetValue();
             first = first.GetNext();
             return temp;
         }
 
+        /// <summary>
+        /// Brings the value of the first element in the queue
+        /// </summary>
+        /// <returns>the first element value</returns>
         public T Head()
         {
-            return first.GetInfo();
+            return first.GetValue();
         }
 
+        /// <summary>
+        /// Provides queue content description
+        /// </summary>
+        /// <returns>string with queue content</returns>
         public override string ToString()
         {
             String s = "<- ";
             for (Node<T> node = first; node != null; node = node.GetNext())
-                s += node.GetInfo() + " ";
+                s += node.GetValue() + " ";
             s += "<-";
             return s;
         }
