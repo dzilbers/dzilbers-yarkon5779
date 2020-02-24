06b421f1fa30b63bd624fa3b5b4ae58f25d29b11
Dan Zilberstein <dzilbers@gmail.com>
2020-02-24 12:01:36 +0200

    last changes

--- BinTree/BinNode.cs
+++ BinTree/BinNode.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BinTree
+﻿namespace BinTree
 {
     public class BinNode<T>
     {
