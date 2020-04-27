e7d9de705cd9b359338545a3253b53d9f348ff9b
Dan Zilberstein <dzilbers@gmail.com>
2020-04-27 12:07:22 +0300

    Hanoi

---
+++ Unit4/HanoiTowers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit4
+{
+    class HanoiTowers
+    {
+        public HanoiTowers(int numOfDiscs)
+        {
+
+        }
+
+        public bool MoveDisc(int fromPoleNum, int toPoleNum)
+        {
+            return false;
+        }
+
+        public int GetNumOfDiscs()
+        {
+            return 0;
+        }
+
+        int GetNumOfDiscs(int poleNum)
+        {
+            return 0;
+        }
+
+        int GetSizeTopDisc(int poleNum)
+        {
+            return 0;
+        }
+
+        bool IsEmpty(int poleNum)
+        {
+            return false;
+        }
+    }
+}
---
+++ Unit4/HanoiTowersApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Unit4.TowerLib;
+
+namespace Unit4
+{
+    class HanoiTowersApplication
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Please enter number of discs[3-10]: ");
+			int numOfDiscs = Int32.Parse(Console.ReadLine));
+
+			Console.Write("Loading simulation...");
+
+			// יצירת עצם מטיפוס מגדלי הנוי עם מספר דיסקיות שהתקבל מהקלט
+			HanoiTowers ht1 = new HanoiTowers(numOfDiscs);
+
+			// הפעלת מגדלי הנוי במצב סימולציה עם השהייה של שנייה אחת  
+			Unit4..HanoiTowersSimulator.simulate(ht1, 1000);
+
+			// להפעלת מגדלי הנוי במצב משחק עם העכבר, החליפו את השורה האחרונה בשורה הבאה  
+			// HanoiTowersSimulator.play(ht1);
+		}
+	}
+}
